a49a7b2160cb1e4ef35df52d0d08eedd4f7c8bd1
Steven Chang <stevenwchang01@gmail.com>
2024-08-30 11:14:49 -0400

    Updated bubble order

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,6 +3,7 @@ function appendInputToChat() {
     userInput = document.getElementById("input").value;
     var temp = `<div class="chat-bubble chat-bubble-user">\
     <div class="chat-bubble-text-user">${userInput}</div></div>`
-    parentDiv.innerHTML += temp;
+    // parentDiv.innerHTML += temp;
+    document.getElementById('result').insertAdjacentHTML('afterbegin',temp);
     document.getElementById("input").value = '';
 }
