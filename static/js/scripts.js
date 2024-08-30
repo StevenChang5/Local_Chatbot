71a2fc94ef4d552dd7e0061d0e1656163c222415
Steven Chang <stevenwchang01@gmail.com>
2024-08-30 11:18:27 -0400

    Updated Javascript

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,9 +1,7 @@
 function appendInputToChat() {
-    const parentDiv = document.getElementById("result");
     userInput = document.getElementById("input").value;
     var temp = `<div class="chat-bubble chat-bubble-user">\
     <div class="chat-bubble-text-user">${userInput}</div></div>`
-    // parentDiv.innerHTML += temp;
     document.getElementById('result').insertAdjacentHTML('afterbegin',temp);
     document.getElementById("input").value = '';
 }
