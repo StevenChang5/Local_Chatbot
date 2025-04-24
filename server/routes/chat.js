e0c61769b95932bdf65832ef768a59ecd5c3f73b
stevendee37 <stevenwchang01@gmail.com>
2025-04-24 12:58:09 -0700

    Fixed not getting response from Ollama

--- server/routes/chat.js
+++ server/routes/chat.js
@@ -4,15 +4,17 @@ const db = require('../db');
 
 router.post('/ask', async (req, res) => {
     const { query } = req.body;
-    console.log('Query from User:', query)
     const response = await fetch('http://localhost:11434/api/generate', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json'},
-        body: JSON.stringify({ model: 'llama3', query })
+        body: JSON.stringify({ 
+            model: 'llama3',
+            prompt: query,
+            stream: false
+        }),
     });
 
     const data = await response.json();
-    console.log('Response from Ollama:', data.response);
     res.json({ response: data.response });
     // console.log(response.message.content);
 
