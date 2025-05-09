d4175ec4645da07c09f14c45ee14f0c121f5985e
stevendee37 <stevenwchang01@gmail.com>
2025-05-08 17:05:50 -0700

    Removed deprecated app version

--- test/static/js/scripts.js
+++
@@ -1,7 +0,0 @@
-function appendInputToChat() {
-    userInput = document.getElementById("input").value;
-    var temp = `<div class="chat-bubble chat-bubble-user">\
-    <div class="chat-bubble-text-user">${userInput}</div></div>`
-    document.getElementById('result').insertAdjacentHTML('afterbegin',temp);
-    document.getElementById("input").value = '';
-}
