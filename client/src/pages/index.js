2e756eb669c977a756f061f631d398e9b3bdbc66
stevendee37 <stevenwchang01@gmail.com>
2025-05-23 15:19:14 -0700

    Updated frontpage styling

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router';
+import './index.css';
 
 const Home = () => {
     return (
-        <div>
-            <h1>Welcome to Local Chatbot!</h1>
-            <Link to="/login">
-                <button>
-                    Login Now!
-                </button>
-            </Link>
+        <div className='main-container'>
+            <div className='start'>
+                <div className='big-header'>Welcome to Local Chatbot!</div>
+                <div className='content'>
+                    Your personal asssistant, with RAG support. Fast and secure. Ask away.
+                </div>
+                <Link to="/login">
+                    <button className='button-link'>
+                        Get Started
+                    </button>
+                </Link>
+            </div>
+            
         </div>
     );
 };
